298a221bcdeb7e78fd0770dedfc8e5fe7cf95495
tatlutgi2018-web <tatlu.tgi2018@gmail.com>
2026-08-12 03:15:31 +0630

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,4 +1,4 @@
-import { supabase } from "../lib/supabase"
+import { supabase } from "@/lib/supabase"
 
 export default async function Home() {
 
@@ -6,23 +6,81 @@ export default async function Home() {
     .from("products")
     .select("*")
 
-  console.log(error)
-
   return (
-    <main>
-      <h1>Electrical Shop</h1>
+    <main style={{
+      padding:"40px",
+      fontFamily:"Arial"
+    }}>
 
-      <p>Our Products</p>
+      <h1>
+        ⚡ Electrical Shop
+      </h1>
 
-      {error && <p>{error.message}</p>}
+      <p>
+        Quality Electrical Products
+      </p>
 
-      {products?.map((product) => (
-        <div key={product.id}>
-          <h2>{product.name}</h2>
-          <p>Brand: {product.brand}</p>
-          <p>Price: {product.price} MMK</p>
+
+      {error && (
+        <p>{error.message}</p>
+      )}
+
+
+      <div style={{
+        display:"grid",
+        gridTemplateColumns:"repeat(auto-fit,minmax(250px,1fr))",
+        gap:"20px",
+        marginTop:"30px"
+      }}>
+
+
+      {products?.map((product)=>(
+
+        <div
+          key={product.id}
+          style={{
+            border:"1px solid #ddd",
+            borderRadius:"15px",
+            padding:"20px",
+            boxShadow:"0 4px 10px rgba(0,0,0,0.1)"
+          }}
+        >
+
+          <h2>
+            {product.name}
+          </h2>
+
+
+          <p>
+            Brand: {product.brand}
+          </p>
+
+
+          <h3>
+            {product.price} MMK
+          </h3>
+
+
+          <button
+            style={{
+              background:"#111",
+              color:"white",
+              padding:"10px 20px",
+              borderRadius:"8px",
+              border:"none"
+            }}
+          >
+            View Product
+          </button>
+
+
         </div>
+
       ))}
+
+
+      </div>
+
 
     </main>
   )
